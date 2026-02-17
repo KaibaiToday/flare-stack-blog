7d85235c971344023c223b88832b1fb3ff3a272b
Zephyrrr <kuangdadu@gmail.com>
2026-02-17 13:04:04 -0800

    feat: Configure release-it to use conventional changelog and set GitHub release name.

--- .release-it.ts
+++ .release-it.ts
@@ -8,10 +8,24 @@ export default {
   },
   github: {
     release: true,
+    releaseName: "v${version}",
   },
   hooks: {
     "before:init": ["bun check", "bun run test"],
     "after:release":
       "echo Successfully released ${name} v${version} to ${repo.repository}.",
+  },
+  plugins: {
+    "@release-it/conventional-changelog": {
+      preset: {
+        name: "conventionalcommits",
+        types: [
+          { type: "feat", section: "Features" },
+          { type: "fix", section: "Bug Fixes" },
+          { type: "refactor", section: "Code Refactoring" },
+        ],
+      },
+      ignoreRecommendedBump: true,
+    },
   },
 } satisfies Config;
